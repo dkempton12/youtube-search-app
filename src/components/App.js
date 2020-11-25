11566886c6cffea08c61b5168b198aa17b3149f0
Dan Kempton <kemptondan@gmail.com>
2020-11-25 18:20:13 -0500

    Styling app background

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
